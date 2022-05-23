cc5747f65a3e2eaf92788b9c42fe44b2c334b73d
Mortheus <cristea.andrei.199@gmail.com>
2022-05-23 20:20:02 +0300

    BSF-13 Create User Database as UserService

---
+++ src/main/java/org/fis/bookstore/services/UserService.java
@@ -0,0 +1,79 @@
+package org.fis.bookstore.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.Cursor;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.dizitart.no2.objects.filters.ObjectFilters;
+import org.fis.bookstore.exceptions.UserNotFoundException;
+import org.fis.bookstore.exceptions.UsernameAlreadyExistsException;
+import org.fis.bookstore.models.User;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
+import static org.fis.bookstore.services.FileSystemService.getPathToFile;
+
+public class UserService {
+
+    private static ObjectRepository<User> userRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("BookStore.db").toFile())
+                .openOrCreate("admin", "admin");
+
+        userRepository = database.getRepository(User.class);
+    }
+
+    public static void addUser(String username, String password, String role) throws UsernameAlreadyExistsException {
+        checkUserDoesNotAlreadyExist(username);
+        userRepository.insert(new User(username, encodePassword(username, password), role));
+    }
+
+    public static User getUser(String username) throws UserNotFoundException {
+        Cursor<User> cursor = userRepository.find(ObjectFilters.eq("username", username));
+        for(User u : cursor){
+            return u;
+        }
+        throw new UserNotFoundException(username);
+    }
+
+    public static void updateUser(User v){
+        userRepository.update(v);
+    }
+
+    public static String getHashedUserPassword(String username) throws UserNotFoundException{
+        return getUser(username).getPassword();
+    }
+
+    private static void checkUserDoesNotAlreadyExist(String username) throws UsernameAlreadyExistsException {
+        for (User user : userRepository.find()) {
+            if (Objects.equals(username, user.getUsername()))
+                throw new UsernameAlreadyExistsException(username);
+        }
+    }
+
+    public static String encodePassword(String salt, String password) {
+        MessageDigest md = getMessageDigest();
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        return new String(hashedPassword, StandardCharsets.UTF_8)
+                .replace("\"", "");
+    }
+
+    private static MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("It fucked up :c");
+        }
+        return md;
+    }
+
+
+}
