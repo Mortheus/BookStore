0fb287c56d48b24b84fbeacf37dd29b1a5884c88
Mortheus <cristea.andrei.199@gmail.com>
2022-05-26 01:16:07 +0300

    BSF-48 Create a service class for Orders

---
+++ src/main/java/org/fis/bookstore/services/ComandaService.java
@@ -0,0 +1,44 @@
+package org.fis.bookstore.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.fis.bookstore.models.Book;
+import org.fis.bookstore.models.Comanda;
+
+import java.util.ArrayList;
+
+import static org.fis.bookstore.services.FileSystemService.getPathToFile;
+
+
+    public class ComandaService {
+
+        private static ObjectRepository<Comanda> comandaRepository;
+
+        private static Nitrite database;
+        private ArrayList<Book> booklist;
+
+        public static void initDatabase() {
+            Nitrite database = Nitrite.builder()
+                    .filePath(getPathToFile("Comanda.db").toFile())
+                    .openOrCreate("admin", "admin");
+            comandaRepository = database.getRepository(Comanda.class);
+        }
+        public static Nitrite getDatabase(){
+            return database;
+        }
+
+        public static void addComanda(String nrTel, String adresa,String username, String status,ArrayList<Book> booklist) {
+            comandaRepository.insert(new Comanda(nrTel, adresa , username, status, booklist));
+        }
+
+
+        public static ArrayList<Comanda> getAll(){
+            ArrayList<Comanda> result = new ArrayList<>();
+            for(Comanda b :  comandaRepository.find()){
+                result.add(b);
+            }
+
+            return result;
+        }
+    }
+
