5ef28ee5524c0aa1d010654e2bbab2500fd3ab7d
Sergiu <bologasergiu22@gmail.com>
2022-05-25 11:50:22 +0300

    BSF-41 Add book not found exception

---
+++ src/main/java/org/fis/bookstore/exceptions/BookNotFoundException.java
@@ -0,0 +1,13 @@
+package org.fis.bookstore.exceptions;
+
+public class BookNotFoundException extends Throwable{
+    private String name;
+    public BookNotFoundException(String name) {
+        super(String.format("The plant does not exist !", name));
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
